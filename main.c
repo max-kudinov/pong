f1b31fd0ea8b6b24f310c7f90753cf6d81bad2d8
mkudinov <m.kudinov@icloud.com>
2024-01-25 18:25:59 +0300

    refactor

--- main.c
+++ main.c
@@ -6,32 +6,41 @@
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
-#define GAP 10
+#define RECT_WIDTH 20
+#define RECT_HEIGHT 100
 
-int main(int argc, char *argv[]) {
+#define RECT_SPEED 25
 
+#define GAP 0
+
+typedef struct {
+    bool j_pressed;
+    bool k_pressed;
+    bool up_pressed;
+    bool down_pressed;
+} key_status;
+
+struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
-    SDL_Rect rect;
+    SDL_Rect player_rect;
+    SDL_Rect computer_rect;
+    key_status keys;
+} state;
 
-    typedef struct {
-        bool h_pressed;
-        bool j_pressed;
-        bool k_pressed;
-        bool l_pressed;
-    }
-    keys;
+void detect_keys(SDL_Scancode scancode, bool pressed);
+int handle_input(SDL_Event *event);
+void render();
 
-    keys kb;
-
+int main(int argc, char *argv[]) {
     // Init SDL2
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't intitalize SDL: %s", SDL_GetError());
         return 1;
     }
 
-    window = SDL_CreateWindow(
+    state.window = SDL_CreateWindow(
         "Game of Pong",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -41,118 +50,107 @@ int main(int argc, char *argv[]) {
         SDL_WINDOW_ALLOW_HIGHDPI
     );
 
-    if (window == NULL) {
+    if (state.window == NULL) {
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-    renderer = SDL_CreateRenderer(
-                    window,
+    state.renderer = SDL_CreateRenderer(
+                    state.window,
                     -1,
                     SDL_RENDERER_PRESENTVSYNC | 
                     SDL_RENDERER_ACCELERATED
                 );
 
-    texture = SDL_CreateTexture(
-                    renderer,
+    state.texture = SDL_CreateTexture(
+                    state.renderer,
                     SDL_PIXELFORMAT_RGBA8888,
                     SDL_TEXTUREACCESS_STATIC,
                     WINDOW_WIDTH,
                     WINDOW_HEIGHT
                 );
 
-    rect.w = 20;
-    rect.h = 100;
+    state.player_rect.w = 20;
+    state.player_rect.h = 100;
 
-    rect.x = 500;
-    rect.y = 500;
+    state.player_rect.x = WINDOW_WIDTH - 70;
+    state.player_rect.y = WINDOW_HEIGHT / 2 - RECT_HEIGHT / 2;
 
-    // The window is opened
-
-
-    SDL_Event e;
+    SDL_Event event;
 
     while (1) {
-        Uint64 start = SDL_GetPerformanceCounter();
 
-        while(SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                SDL_Log("Program quit after %i ticks", e.quit.timestamp);
-                SDL_DestroyRenderer(renderer);
-                SDL_DestroyWindow(window);
-                SDL_Quit();
-                return 0;
-            }
-
-            SDL_Scancode key = e.key.keysym.scancode;
-
-            if (e.type == SDL_KEYDOWN) {
-
-                if (key == SDL_SCANCODE_K) {
-                    kb.k_pressed = true;
-                }
-
-                else if (key == SDL_SCANCODE_J) {
-                    kb.j_pressed = true;
-                }
-
-                else if (key == SDL_SCANCODE_L) {
-                    kb.l_pressed = true;
-                }
-
-                else if (key == SDL_SCANCODE_H) {
-                    kb.h_pressed = true;
-                }
-            }
-
-            if (e.type == SDL_KEYUP) {
-
-                if (key == SDL_SCANCODE_K) {
-                    kb.k_pressed = false;
-                }
-
-                else if (key == SDL_SCANCODE_J) {
-                    kb.j_pressed = false;
-                }
-
-                else if (key == SDL_SCANCODE_L) {
-                    kb.l_pressed = false;
-                }
-
-                else if (key == SDL_SCANCODE_H) {
-                    kb.h_pressed = false;
-                }
-            }
-
-            if (kb.k_pressed && rect.y > GAP) {
-                    rect.y -= 10;
-            }
-
-            else if (kb.j_pressed && rect.y + rect.h + GAP < WINDOW_HEIGHT) {
-                    rect.y += 10;
-            }
-
-            else if (kb.l_pressed && rect.x + rect.w + GAP < WINDOW_WIDTH) {
-                    rect.x += 10;
-            }
-
-            else if (kb.h_pressed && rect.x > GAP) {
-                    rect.x -= 10;
-            }
+        if (handle_input(&event)) {
+            return 1;
         }
 
+        if ((state.keys.k_pressed || state.keys.up_pressed) && state.player_rect.y > 0) {
+            state.player_rect.y -= RECT_SPEED;
+        }
 
-        SDL_SetRenderTarget(renderer, texture);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
-        SDL_RenderDrawRect(renderer, &rect);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-        SDL_RenderFillRect(renderer, &rect);
-        SDL_RenderPresent(renderer);
-
-        Uint64 end = SDL_GetPerformanceCounter();
-
-        int fps = 1.0 / ((end - start) / (float) SDL_GetPerformanceFrequency());
-        printf("FPS: %i\n", fps);
+        else if ((state.keys.j_pressed || state.keys.down_pressed) &&
+                  state.player_rect.y + RECT_HEIGHT < WINDOW_HEIGHT) {
+            state.player_rect.y += RECT_SPEED;
+        }
+        render();
     }
+}
+
+void detect_keys(SDL_Scancode scancode, bool pressed) {
+    switch (scancode) {
+        case SDL_SCANCODE_UP:
+            state.keys.up_pressed = pressed;
+            break;
+
+        case SDL_SCANCODE_DOWN:
+            state.keys.down_pressed = pressed;
+            break;
+
+        case SDL_SCANCODE_J:
+            state.keys.j_pressed = pressed;
+            break;
+
+        case SDL_SCANCODE_K:
+            state.keys.k_pressed = pressed;
+            break;
+
+        default:
+            break;
+    }
+}
+
+int handle_input(SDL_Event *event) {
+
+    while(SDL_PollEvent(event)) {
+
+        SDL_Scancode scancode = event->key.keysym.scancode;
+
+        switch(event->type) {
+            case SDL_QUIT:
+                SDL_Log("Program quit after %i ticks", event->quit.timestamp);
+                SDL_DestroyRenderer(state.renderer);
+                SDL_DestroyWindow(state.window);
+                SDL_Quit();
+                return 1;
+
+            case SDL_KEYDOWN:
+                detect_keys(scancode, true);
+                break;
+
+            case SDL_KEYUP:
+                detect_keys(scancode, false);
+                break;
+        }
+    }
+    return 0;
+}
+
+void render() {
+    SDL_SetRenderTarget(state.renderer, state.texture);
+    SDL_SetRenderDrawColor(state.renderer, 0, 0, 0, 0);
+    SDL_RenderClear(state.renderer);
+    SDL_RenderDrawRect(state.renderer, &state.player_rect);
+    SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 0);
+    SDL_RenderFillRect(state.renderer, &state.player_rect);
+    SDL_RenderPresent(state.renderer);
 }
